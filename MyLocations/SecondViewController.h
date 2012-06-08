5e128df24db04f96b1a71d9f97603dc7a789414d
Calvin Cheng <calvin@calvinx.com>
2012-06-09 01:18:12 +0800

    Initial Commit

---
+++ MyLocations/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MyLocations
+//
+//  Created by Calvin Cheng on 9/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
